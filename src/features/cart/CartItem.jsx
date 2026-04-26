3bb2766754ae886f4fc9c0bd0074afd558f8355d
Ahmed Adel <ahmedadel0239@gmail.com>
2026-04-26 08:13:04 +0300

    feat: adjust layout for CartItem component to improve spacing

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -13,7 +13,7 @@ function CartItem({ item }) {
       <p className="mb-1 sm:mb-0">
         {quantity}&times; {name}
       </p>
-      <div className="flex items-center justify-between sm:gap-6">
+      <div className="flex items-center justify-between flex-wrap gap-3 sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
 
         <UpdateItemQuantity
